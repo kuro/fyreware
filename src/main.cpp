edeb04bd8dadf1a91494873ef90db7a4309037d7
blanton <theblanton@gmail.com>
2010-09-30 14:09:28 -0400

    an empty application

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+
+/**
+ * @file main.cpp
+ * @brief main implementation
+ */
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication app (argc, argv);
+    return 0;
+}
